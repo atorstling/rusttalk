4963d67e6f1894ff45efc2284815d90ed479d82b
Alexander Torstling <alexander@torstling.com>
2016-10-20 21:57:53 +0200

    scoped threads with crossbeam

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,21 @@
-extern crate rustc_serialize;
+extern crate crossbeam;
 
-use rustc_serialize::json;
-//use std::thread;
-
-fn fork<F1,R1,F2,R2>(f1: F1, f2: F2) -> (R1, R2) where F1: FnOnce() -> R1, F2: FnOnce() -> R2 {
-  /*
-  let t1 = thread::spawn(move || {
-    f1();
-  });
-  let t2 = thread::spawn(move || {
-    f2();
-  });
-  (t1.join().unwrap(), t2.join().unwrap());
-  */
-  (f1(), f2())
+fn fork<'a,F1,R1,F2,R2>(f1: F1, f2: F2) -> (R1, R2) where 
+  F1: FnOnce() -> R1 + Send + 'a, 
+  F2: FnOnce() -> R2 + Send + 'a,
+  R1: Send + 'a,
+  R2: Send + 'a {
+  crossbeam::scope(|scope| {
+  let t1 = scope.spawn(move || { f1() });
+  let t2 = scope.spawn(move || { f2() });
+  (t1.join(), t2.join())
+  })
 }
 
 #[test]
-fn test_what() {
+fn test_fork() {
   let (a, b) = fork(|| 1, || 2);
   assert!(a == 1);
   assert!(b == 2);
 }
 
-fn main() {
-    println!("{:?}", json::encode(&42));
-    println!("{:?}", json::encode(&vec!["to", "be", "or", "not", "to", "be"]));
-    println!("{:?}", json::encode(&Some(true)));
-}
