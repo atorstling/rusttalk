78bb411c394848d1cdba532447351d1f8709f029
Alexander Torstling <alexander.torstling@bisnode.com>
2017-08-11 18:16:04 +0200

    specify tmp dir for os x docker mount reasons

--- slides/run_server.sh
+++ slides/run_server.sh
@@ -8,8 +8,11 @@
 # ./build.sh or
 # TOOLS_TO_BUILD= CHANNELS_TO_BUILD=nightly ./build.sh (for only nightly, no tools)
 dir=~/projects/private/rust-playground/ui 
+tmpdir=/tmp/playground
+mkdir -p $tmpdir
 cd $dir
 #TMPDIR=/mnt/playground \
+TMPDIR=$tmpdir \
 RUST_LOG=ui=debug \
 PLAYGROUND_UI_ADDRESS=0.0.0.0 \
 PLAYGROUND_UI_PORT=4711 \
